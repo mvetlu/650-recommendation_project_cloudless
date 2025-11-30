690ae9cc89eff44210e0325df23b4cd7e09fa9e8
Nadiyahlw <Nadiyah_L_Williams@yahoo.com>
2025-11-30 16:06:57 -0500

    fixing path

--- analysis/record_cloud_metrics.py
+++ analysis/record_cloud_metrics.py
@@ -5,8 +5,7 @@ import statistics
 
 OUTPUT_CSV = "system_comparison_metrics.csv"
 
-# TODO: replace with your actual EC2 public IP
-API_URL = "http://18.191.229.202:8000/recommend/1" 
+API_URL = "http://YOUR_EC2_IP:8000/recommend/1" 
 
 
 def measure_cloud_metrics(n_requests=200):
